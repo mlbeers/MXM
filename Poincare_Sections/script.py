24f31764cfd7300360c21caf47b43a0d4236ca65
mlbeers <mlbeers@math.wisc.edu>
2024-09-06 15:27:03 -0500

    run script to find distribution for given shape

--- Poincare_Sections/script.py
+++ Poincare_Sections/script.py
@@ -24,97 +24,96 @@ import sys
 #number of squares for STS
 n_squares = int(sys.argv[1])
 #index to start at
-start_index = int(sys.argv[2])
-#number of squares to skip count by if we are running the script in multiple tmux windows
-increment = int(sys.argv[3])
+index = int(sys.argv[2])
 
 permutations = perms_list(n_squares)
 
-#go through all permutations
-for index in range(start_index, len(permutations), increment):
+print("\n\n INDEX - " + str(index) + "\n\n")
 
-    print("\n\n INDEX - " + str(index) + "\n\n")
-    
-    perm = permutations[index]
+perm = permutations[index]
 
-    # get a list of saddle connections
-    vec_file = "vecs" + str(n_squares) + "-" + str(index) + ".npy"
-    vecs0 = load_arrays_from_file(os.path.join("vecs", vec_file))
-    
-    #generate a list of alpha, c matrices, and eigenvectors for each cusp of the STS to experiment with to find "nice" sections for our poincare sections
-    a = []
-    c = []
-    e = []
-    g = []
-    for num in range(50):
-        try:
-            alphas, Cs, C_invs, eigs, Ms, generators, eigenvecs = poincare_details(perm, vecs0)
-        except:
+# get a list of saddle connections
+vec_file = "vecs" + str(n_squares) + "-" + str(index) + ".npy"
+vecs0 = load_arrays_from_file(os.path.join("vecs", vec_file))
+
+print(len(vecs0))
+
+#generate a list of alpha, c matrices, and eigenvectors for each cusp of the STS to experiment with to find "nice" sections for our poincare sections
+a = []
+c = []
+e = []
+g = []
+for num in range(200):
+    try:
+        alphas, Cs, C_invs, eigs, Ms, generators, eigenvecs = poincare_details(perm, vecs0)
+    except:
+        continue
+    a.append(alphas)
+    c.append(Cs)
+    e.append(eigenvecs)
+    g.append(generators)
+print(len(a))
+
+#write these values to a file
+data = [a, c, e, g]
+with open(os.path.join("results", f"{n_squares} - {index}", "setup.dill"), 'wb') as f:
+    dill.dump(data, f)
+
+dx = 0.0005
+covolume_list = []
+
+#go thorugh all cusps
+for j in range(len(a[0])):
+
+    #change back to False in future
+    improved = True
+    if j == 0:
+        improved = True
+        
+    for i in range(len(a)):
+
+        #get dimensions of section 
+        vecs, x_vals, m0, m1, x0, y0, dx_y, z = setup(a[i][j], c[i][j], e[i][j], vecs0, dx, improved)
+        print(z)
+        print("i = " + str(i), "j = " + str(j))
+
+        if float(z) <= float(1/50000):
+            print("too small")
             continue
-        a.append(alphas)
-        c.append(Cs)
-        e.append(eigenvecs)
-        g.append(generators)
 
-    #write these values to a file
-    data = [a, c, e, g]
-    with open(os.path.join("results", f"{n_squares} - {index}", "setup.dill"), 'wb') as f:
-        dill.dump(data, f)
-    
-    dx = 0.0005
-    covolume_list = []
+        #create a dataframe with winning vector at certain points in the section
+        df = winners(vecs, x_vals, m0, m1, y0, dx, dx_y)
 
-    #go thorugh all cusps
-    for j in range(len(a[0])):
-       
-        improved = False
-        if j == 0:
-            improved = True
+        #plot poincare section and save
+        try:
+            plot(df, vecs, c[i][j], j, n_squares, index, test = False)
+        except Exception as error:
+            print(error)
+            continue
+        df.to_csv(os.path.join("results", f"{n_squares} - {index}", "df - " + str(j)), index=False)
+
+        #make section object that define winning vector and line equations for boundaries of subsections
+        sec_list = sec_setup(df, dx_y)
+        secs = sec_comp(sec_list, dx)
+        with open(os.path.join("results", f"{n_squares} - {index}", "secs - " + str(j) + ".dill"), 'wb') as f:
+            dill.dump(secs, f)
             
-        for i in range(len(a)):
+        times = [1]
+        if improved:
+            times = time_comp(secs)
 
-            #get dimensions of section 
-            vecs, x_vals, m0, m1, x0, y0, dx_y, z = setup(a[i][j], c[i][j], e[i][j], vecs0, dx, improved)
-            print(z)
-            print("i = " + str(i), "j = " + str(j))
-    
-            if float(z) <= float(1/50000):
-                print("too small")
-                continue
+        #plot the pdf for each cusp
+        pdf(list(df["time"]), times, dx*2, n_squares, index, j)
 
-            #create a dataframe with winning vector at certain points in the section
-            df = winners(vecs, x_vals, m0, m1, y0, dx, dx_y)
+        #get covolume calculations
+        covolume_list.append(covolume(secs))
+        print("section done")
 
-            #plot poincare section and save
-            try:
-                plot(df, vecs, c[i][j], j, n_squares, index, test = False)
-            except Exception as error:
-                print(error)
-                continue
-            df.to_csv(os.path.join("results", f"{n_squares} - {index}", "df - " + str(j)), index=False)
+        break
 
-            #make section object that define winning vector and line equations for boundaries of subsections
-            sec_list = sec_setup(df, dx_y)
-            secs = sec_comp(sec_list, dx)
-            with open(os.path.join("results", f"{n_squares} - {index}", "secs - " + str(j) + ".dill"), 'wb') as f:
-                dill.dump(secs, f)
-                
-            times = [1]
-            if improved:
-                times = time_comp(secs)
-
-            #plot the pdf for each cusp
-            pdf(list(df["time"]), times, dx*2, n_squares, index, j)
-
-            #get covolume calculations
-            covolume_list.append(covolume(secs))
-            print("section done")
-    
-            break
-    
-    with open(os.path.join("results", f"{n_squares} - {index}", "covolume.txt"), "w") as file:
-        total = 0
-        for item in covolume_list:
-            total += item
-            file.write(str(item) + "\n")
-        file.write("\n" + str(total))
+with open(os.path.join("results", f"{n_squares} - {index}", "covolume.txt"), "w") as file:
+    total = 0
+    for item in covolume_list:
+        total += item
+        file.write(str(item) + "\n")
+    file.write("\n" + str(total))
