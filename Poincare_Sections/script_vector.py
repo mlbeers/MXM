af1f751acd81a2988adc3f38ab2f5bdd0792c7ce
Gabriela Brown <gabcbrown@gmail.com>
2024-09-27 16:04:10 -0500

    formatting with autopep8

--- Poincare_Sections/script_vector.py
+++ Poincare_Sections/script_vector.py
@@ -11,7 +11,8 @@ import pandas as pd
 import re
 import os
 
-def vectors(h, v, length = 256):
+
+def vectors(h, v, length=256):
     from flatsurf import translation_surfaces
     S = SymmetricGroup(len(h))
     T = translation_surfaces.origami(S(h), S(v))
@@ -20,26 +21,28 @@ def vectors(h, v, length = 256):
     TT = to_pyflatsurf(T)
     from pyflatsurf import flatsurf
     C = TT.connections().bound(flatsurf.Bound(length))
-    connections = [vector((ZZ(str(c.vector().x())), ZZ(str(c.vector().y())))) for c in C]
+    connections = [
+        vector((ZZ(str(c.vector().x())), ZZ(str(c.vector().y())))) for c in C]
     vecs = []
     for vec in connections:
-        item = np.array([[vec[0]],[vec[1]]])
+        item = np.array([[vec[0]], [vec[1]]])
         vecs.append(item)
     print(len(vecs))
     return vecs
-    
-#number of squares for STS
+
+
+# number of squares for STS
 n_squares = int(sys.argv[1])
-#index to start at
+# index to start at
 start_index = int(sys.argv[2])
-#number of squares to skip count by if we are running the script in multiple tmux windows
+# number of squares to skip count by if we are running the script in multiple tmux windows
 increment = int(sys.argv[3])
 
-#list of permuatations
+# list of permuatations
 permutations = perms_list(n_squares)
 
 for i in range(start_index, len(permutations), increment):
-    #get the horizontal and vertical permutations
+    # get the horizontal and vertical permutations
     h = str(permutations[i].r())
     v = str(permutations[i].u())
     for num in range(1, n_squares+1):
@@ -47,7 +50,7 @@ for i in range(start_index, len(permutations), increment):
             h += "(" + str(num) + ")"
         if str(num) not in v:
             v += "(" + str(num) + ")"
-    #generate saddle connections on surface and save file
+    # generate saddle connections on surface and save file
     name = "vecs" + str(n_squares) + "-" + str(i) + ".npy"
     vecs0 = vectors(h, v, 2000)
     save_arrays_to_file(os.path.join("vecs", name), vecs0)
