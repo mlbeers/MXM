b108bd014eb165482c47ddd6bcfb63a502bc4237
Gabriela Brown <gabcbrown@gmail.com>
2024-09-27 16:30:13 -0500

    fixing the file names

--- Poincare_Sections/script_vector.py
+++ Poincare_Sections/script_vector.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from Library import *
 from Library import Section
+from .vector import generate_vectors
 import pandas as pd
 from time import time
 from flatsurf import *
@@ -10,25 +11,6 @@ import sys
 import pandas as pd
 import re
 import os
-
-
-def vectors(h, v, length=256):
-    from flatsurf import translation_surfaces
-    S = SymmetricGroup(len(h))
-    T = translation_surfaces.origami(S(h), S(v))
-    T = T.erase_marked_points()
-    from flatsurf.geometry.pyflatsurf_conversion import to_pyflatsurf
-    TT = to_pyflatsurf(T)
-    from pyflatsurf import flatsurf
-    C = TT.connections().bound(flatsurf.Bound(length))
-    connections = [
-        vector((ZZ(str(c.vector().x())), ZZ(str(c.vector().y())))) for c in C]
-    vecs = []
-    for vec in connections:
-        item = np.array([[vec[0]], [vec[1]]])
-        vecs.append(item)
-    print(len(vecs))
-    return vecs
 
 
 # number of squares for STS
@@ -52,5 +34,5 @@ for i in range(start_index, len(permutations), increment):
             v += "(" + str(num) + ")"
     # generate saddle connections on surface and save file
     name = "vecs" + str(n_squares) + "-" + str(i) + ".npy"
-    vecs0 = vectors(h, v, 2000)
+    vecs0 = generate_vectors(h, v, 2000)
     save_arrays_to_file(os.path.join("vecs", name), vecs0)
