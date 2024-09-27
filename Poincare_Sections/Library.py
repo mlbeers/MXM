995c209d61203816d3b2da83b1738c5029b569a7
Gabriela Brown <gabcbrown@gmail.com>
2024-09-27 16:24:00 -0500

    fix formatting

--- Poincare_Sections/Library.py
+++ Poincare_Sections/Library.py
@@ -295,35 +295,35 @@ def winners(vecs, x_vals, m0, m1, y0, dx, dx_y):
     print("top done: " + str(t1 - t0))
 
     # could re-include if we need to do computations on all sides
-    # diagonal
+    # # diagonal
     # for a in np.arange(0 + dz, 1, dz):
-    # check = 0
-    # winner_slope = None
-    # winner = None
-    # Mab = np.array([[a, m1*a + 1-dx/y0 + dx_y], [0, a]])
-    # for vec in vecs:
-    # new = Mab@vec
-    # if float(new[0][0]) == 0:
-    # continue
-    # x = float(new[0][0])
-    # y = float(new[1][0])
-    # if y/x <= 0:
-    # continue
-    # if x <= 1 and x > 0:
-    # if winner_slope == None:
-    # winner_slope = y/x
-    # winner = vec
-    # continue
-    # if you have two potential winners like (m,n) and 2*(m,n), make (m,n) winner for continuity and plotting purposes
-    # elif abs(y/x - winner_slope) <= dx/1000:
-    # if vec[0][0] < winner[0][0] or vec[1][0] < winner[1][0]:
-    # winner = vec
-    # continue
-    # elif y/x < winner_slope:
-    # winner_slope = y/x
-    # winner = vec
-    # continue
-    # winners.append(winner)
+    #     check = 0
+    #     winner_slope = None
+    #     winner = None
+    #     Mab = np.array([[a, m1*a + 1-dx/y0 + dx_y], [0, a]])
+    #     for vec in vecs:
+    #         new = Mab@vec
+    #         if float(new[0][0]) == 0:
+    #             continue
+    #         x = float(new[0][0])
+    #         y = float(new[1][0])
+    #         if y/x <= 0:
+    #             continue
+    #         if x <= 1 and x > 0:
+    #             if winner_slope == None:
+    #                 winner_slope = y/x
+    #                 winner = vec
+    #                 continue
+    #    # if you have two potential winners like (m,n) and 2*(m,n), make (m,n) winner for continuity and plotting purposes
+    #             elif abs(y/x - winner_slope) <= dx/1000:
+    #                 if vec[0][0] < winner[0][0] or vec[1][0] < winner[1][0]:
+    #                     winner = vec
+    #                     continue
+    #             elif y/x < winner_slope:
+    #                 winner_slope = y/x
+    #                 winner = vec
+    #                 continue
+    #     winners.append(winner)
 
     # side edge
     t0 = time()
